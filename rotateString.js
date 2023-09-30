cf1dec00376eb7d18afe3f46f9cbcdcbe35b38b7
Dheeraj Kumar <55842638+Dheeraj12121@users.noreply.github.com>
2023-09-30 21:25:59 +0530

    rotateString

---
+++ rotateString.js
@@ -0,0 +1,7 @@
+var rotateStirng = function(A, B) {
+    if(A.length !== B.length) return false;
+    if(A.length === 0 && B.length === 0) return true;
+
+    A = A + A;
+    return A.includes(B);
+}
